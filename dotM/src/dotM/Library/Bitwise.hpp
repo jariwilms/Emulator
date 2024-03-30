cf2237f9d02f8af8d7de0bae442276ff2dcd343b
Jari Wilms <jariwilms@outlook.com>
2024-03-30 16:01:32 +0100

    Moved CPU bitwise methods to Bitwise file

--- dotM/src/dotM/Library/Bitwise.hpp
+++ dotM/src/dotM/Library/Bitwise.hpp
@@ -47,3 +47,55 @@ constexpr inline T set_bits(const T& value, U index, U count, const V& src)
 
 	return (value & ~((((U{} + 1) << count) - 1) << index)) | (src << index);
 }
+
+template <typename T, size_t MSB>
+static constexpr T sign_extend(const T& value)
+{
+    struct extend { T v : MSB; };                                       //Let the compiler handle it
+
+    return extend{ value }.v;
+}
+
+template<typename T>
+static constexpr T bit_reverse(const T& value)
+{
+    static_assert(std::is_integral<T>::value, "T must be an integral type!");
+
+    T result{};
+    constexpr auto size = sizeof(T) * 8;
+
+    for (auto i = 0; i < size; ++i)
+    {
+        result |= get_bit(value, i) << (size - i - 1);
+    }
+
+    return result;
+}
+
+enum class ShiftType
+{
+    LSL = 0b00,                                                                //Logical Shift Left
+    LSR = 0b01,                                                                //Logical Shift Right
+    ASR = 0b10,                                                                //Arithmetic Shift Right
+    ROR = 0b11,                                                                //Rotate Right
+};
+template<typename T, typename U>
+static constexpr T shift(T value, ShiftType type, U bits)
+{
+    static_assert(std::is_integral<T>::value, "T must be an integral type!");
+    static_assert(std::is_integral<U>::value, "U must be an integral type!");
+
+
+
+    constexpr auto size = sizeof(T) * 8;
+
+    switch (type)
+    {
+        case ShiftType::LSL: return value << bits;
+        case ShiftType::LSR: return value >> bits;
+        case ShiftType::ASR: return get_bit(value, size - 1) ? ~(~value >> bits) : value >> bits;
+        case ShiftType::ROR: return (value >> bits) | (value << (size - bits));
+
+        default:             throw std::runtime_error("Invalid ShiftType!");
+    }
+}
