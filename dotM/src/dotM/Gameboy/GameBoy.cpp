875f49233d288167c11f23415352a45e7c6efa73
Jari Wilms <jariwilms@outlook.com>
2024-04-02 17:24:15 +0200

    GameBoy class

--- dotM/src/dotM/Gameboy/GameBoy.cpp
+++ dotM/src/dotM/Gameboy/GameBoy.cpp
@@ -2,3 +2,29 @@
 
 #include "GameBoy.hpp"
 
+namespace dot::gb
+{
+    void GameBoy::on()
+    {
+        boot();
+
+        m_cpu = std::make_unique<SharpSM83>(m_memory);
+
+        for (int i = 0; i < 10; ++i)
+            m_cpu->cycle();
+    }
+
+    void GameBoy::off()
+    {
+
+    }
+
+    void GameBoy::boot()
+    {
+        const auto& bios = read_file(BIOS_LOC);
+        std::memcpy(m_memory.data(), bios.data(), bios.size());
+
+        const auto& pak = read_file(PAK_LOC);
+        std::memcpy(m_memory.data() + 0x100, pak.data(), pak.size());
+    }
+}
--- dotM/src/dotM/Gameboy/GameBoy.hpp
+++ dotM/src/dotM/Gameboy/GameBoy.hpp
@@ -2,13 +2,21 @@
 
 #include <stdafx.hpp>
 
+#include "dotM/Gameboy/Specification/GB.hpp"
+
 namespace dot::gb
 {
     class GameBoy
     {
     public:
 
-    private:
+        void on();
+        void off();
 
+    private:
+        void boot();
+
+        std::unique_ptr<SharpSM83> m_cpu{};
+        std::array<byte, 0xFFFF> m_memory{};
     };
 }
